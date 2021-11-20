75933a256bdabdce0718dd989a697bbfa6bcebe5
Limbad Yash <limbadyash1@pop-os.localdomain>
2021-11-20 11:21:28 +0530

    Added message block in html and fixed slideshow

--- static/index.js
+++ static/index.js
@@ -1,4 +1,8 @@
 var slideIndex = 0;
+
+/* 
+  -------------- Automatic slideshow ----------------
+*/
 showSlides();
 
 function showSlides() {
@@ -11,4 +15,36 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
+/*
+  ----------------- manual slideshow ----------------
+*/
+
+showSlide(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlide(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlide(slideIndex = n);
+}
+
+function showSlide(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
